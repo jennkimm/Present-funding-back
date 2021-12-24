01713f61ae1aaf6d546aaaa1ace12575cf874622
jennkimm <seohkim@student.42seoul.kr>
2021-12-24 15:43:04 +0900

    initial commit

---
+++ src/main/java/com/example/pfunding/PfundingApplication.java
@@ -0,0 +1,13 @@
+package com.example.pfunding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PfundingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PfundingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pfunding/PfundingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pfunding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PfundingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
