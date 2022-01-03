220997aad7129a26686f90ceea7a319a727d1cd2
jennkimm <seohkim@student.42seoul.kr>
2022-01-03 23:26:55 +0900

    add social login

---
+++ src/main/java/com/example/pfunding/model/AuthProvider.java
@@ -0,0 +1,8 @@
+package com.example.pfunding.model;
+
+public enum AuthProvider {
+    local,
+    google,
+    facebook,
+    naver
+}
---
+++ src/main/java/com/example/pfunding/security/CustomUserDetailsService.java
@@ -0,0 +1,40 @@
+package com.example.pfunding.security;
+
+import com.example.pfunding.exception.ResourceNotFoundException;
+import com.example.pfunding.model.User;
+import com.example.pfunding.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String email)
+            throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User not found with email : " + email)
+                );
+
+        return UserPrincipal.create(user);
+    }
+
+    @Transactional
+    public UserDetails loadUserById(Long id) {
+        User user = userRepository.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException("User", "id", id)
+        );
+
+        return UserPrincipal.create(user);
+    }
+}
